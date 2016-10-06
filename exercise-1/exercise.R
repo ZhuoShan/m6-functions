94bbd86f8b44e20858ac9c0beb9a071e999dc38f
Stanley <apple@D-173-250-161-205.dhcp4.washington.edu>
2016-10-06 14:45:02 -0700

    Second Commit

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,18 +1,18 @@
 # Exercise 1: writing and executing functions
-
 # Write a function `AddThree` that adds 3 to an input value
-AddThree <- function(value)
+AddThree <- function(value) {
   result <- value + 3
 return(result)
-
+}
 # Create a variable `ten` by passing 7 to your `AddThree` function
 ten <- AddThree(7)
 
 # Write a function `FeetToMeters` that converts from feet to meters
 
-FeetToMeters <- function(value)
+FeetToMeters <- function(value) {
   result <- value * 0.3048
 return(result)
+}
 # Create a variable `height.in.feet` that is your height in feet
 height.in.feet <- 6.1
 
